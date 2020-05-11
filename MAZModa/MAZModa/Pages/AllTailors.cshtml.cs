6da8751ddc5451c726b488708108053a69c2c24a
Alzahraa-Omar <alzahraa.o.gaber@gmail.com>
2020-05-11 21:22:20 +0200

    Edit pages

---
+++ MAZModa/MAZModa/Pages/AllTailors.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MAZModa.Data;
+using MAZModa.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MAZModa.Pages
+{
+    public class AllTailorsModel : PageModel
+    {
+        MAZModaDbContext context = new MAZModaDbContext();
+        public void OnGet()
+        {
+            ICollection<Tailor> AllTailors = context.Tailor.ToList();
+            //ICollection<ICollection<Photo>> TailorPhotoLists=
+        }
+    }
+}
